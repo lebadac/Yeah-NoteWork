68ae8aa290285b152617a721926ed9a56bf7cfa1
TUYETMAI\HP <duongmai6241@gmail.com>
2024-06-03 08:32:17 +0700

    hinh nhu xong roi

--- server/models/Notes.js
+++ server/models/Notes.js
@@ -2,12 +2,7 @@ module.exports = (sequelize, DataTypes) => {
   const Notes = sequelize.define(
     "Notes",
     {
-    //   ID: {
-    //     type: DataTypes.INTEGER, // Corrected to lowercase "integer"
-    //     allowNull: false,
-    //     primaryKey: true,
-    //     autoIncrement: true,
-    //   },
+
       TaskName: {
         type: DataTypes.STRING,
         allowNull: false,
